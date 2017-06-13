26651f407e201b40ff25a43402787c80b0dc802c
garethquirke <gareth.quirke@gmail.com>
2017-06-13 12:32:10 +0100

    start script

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 """
 main file for the service
 """
-
 from flask import Flask, render_template
-# name : __main__
+
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def welcome():
-    # return 'gonch'
     return render_template('home.html')
