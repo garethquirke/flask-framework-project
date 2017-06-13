dc466baa4d86dd267bde635b931e5f7f50119175
garethquirke <gareth.quirke@gmail.com>
2017-06-13 01:33:39 +0100

    main page, bootstrap, jquery added

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 """
 main file for the service
 """
-from flask import Flask
 
+from flask import Flask, render_template
+# name : __main__
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def welcome():
-    return 'welcome to the site'
+    # return 'gonch'
+    return render_template('home.html')
