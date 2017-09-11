1eed467c102bb14259c4b96b0941a2d5e2c4dd61
garethquirke <gareth.quirke@gmail.com>
2017-09-11 21:24:05 +0100

    wtf forms

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 """
 main file for the service
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
 from data import OurGuitars
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 Guitars = OurGuitars()
 
