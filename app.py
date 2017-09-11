48e532b0b209f806cfeeea8555b9bab8d2f06d00
garethquirke <gareth.quirke@gmail.com>
2017-09-11 21:01:54 +0100

    browse menu complete

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 main file for the service
 """
 from flask import Flask, render_template
+from data import OurGuitars
+
+Guitars = OurGuitars()
+
 
 app = Flask(__name__)
 app.debug = True
@@ -9,3 +13,12 @@ app.debug = True
 @app.route('/')
 def welcome():
     return render_template('home.html')
+
+@app.route('/about')
+def about():return render_template('about.html')
+
+@app.route('/browse')
+def browse():return render_template('browse.html', guitars = Guitars)
+
+@app.route('/guitar/<string:ID>')
+def viewitem(ID):return render_template('guitar.html', id = ID)
---
+++ data.py
@@ -0,0 +1,28 @@
+def OurGuitars():
+    guitars = [
+        {
+            "ID" : 1,
+            "Name" : "Fender Strat",
+            "Price" : 460,
+            "Quantity" : 6
+        },
+           {
+            "ID" : 2,
+            "Name" : "Fender Tele",
+            "Price" : 330,
+            "Quantity" : 4
+        },
+           {
+            "ID" : 3,
+            "Name" : "Gibson Hummingbird",
+            "Price" : 380,
+            "Quantity" : 11
+        },
+           {
+            "ID" : 4,
+            "Name" : "Fender Jazzmaster",
+            "Price" : 340,
+            "Quantity" : 4
+        }
+    ]
+    return guitars
